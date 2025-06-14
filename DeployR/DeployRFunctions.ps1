aa23afee732367a1f706951a33245da078794977
Gary Blok <gary@thebloks.com>
2025-06-13 20:13:11 -0500

    25.6.13.20.13

--- DeployR/DeployRFunctions.ps1
+++ DeployR/DeployRFunctions.ps1
@@ -2,6 +2,8 @@
 
 #Functions
 
+Write-Host "Loading DeployR Functions" -ForegroundColor Cyan
+write-host "Function: Get-DeployRGather" -ForegroundColor Green
 function Get-DeployRGather {
     iex (irm "https://gather.garytown.com")
 }
