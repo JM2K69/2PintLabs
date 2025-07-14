17efba60bdbe4091ce850daef4c257f1e8a1ba49
Gary Blok <gary@thebloks.com>
2025-07-14 16:29:25 -0500

    16.29

--- DeployR/TSScripts/StepDefinitions/Invoke-PSLenovoUpdaterPWSH.ps1
+++ DeployR/TSScripts/StepDefinitions/Invoke-PSLenovoUpdaterPWSH.ps1
@@ -1,10 +1,4 @@
 write-host -ForegroundColor Cyan "Starting Invoke-PSLenovoUpdater..."
-write-host "Start-Process -FilePath `"C:\Program Files\PowerShell\7\pwsh.exe`" -ArgumentList `"-NoProfile -ExecutionPolicy Bypass -File `"$PSScriptRoot\Invoke-PSLenovoUpdater.ps1`"`""
+write-host 'cmd.exe /c pwsh.exe -NoProfile -ExecutionPolicy Bypass -File .\Invoke-PSLenovoUpdater.ps1 -Wait -NoNewWindow -PassThru '
 
-Start-Process -FilePath "C:\Program Files\PowerShell\7\pwsh.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSScriptRoot\Invoke-PSLenovoUpdater.ps1`"" -Wait -NoNewWindow -PassThru 
-if ($LASTEXITCODE -ne 0) {
-    Write-Host -ForegroundColor Red "Invoke-PSLenovoUpdater failed with exit code $LASTEXITCODE."
-    exit $LASTEXITCODE
-} else {
-    Write-Host -ForegroundColor Green "Invoke-PSLenovoUpdater completed successfully."
-}
+cmd.exe /c pwsh.exe -NoProfile -ExecutionPolicy Bypass -File .\Invoke-PSLenovoUpdater.ps1 -Wait -NoNewWindow -PassThru 
