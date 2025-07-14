e337589d16759ef35c169fba7b9e7207e6169391
Gary Blok <gary@thebloks.com>
2025-07-14 15:36:33 -0500

    15.36

--- DeployR/TSScripts/StepDefinitions/Invoke-PSLenovoUpdaterPWSH.ps1
+++ DeployR/TSScripts/StepDefinitions/Invoke-PSLenovoUpdaterPWSH.ps1
@@ -1,6 +1,10 @@
-Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSScriptRoot\Invoke-PSLenovoUpdater.ps1`"" -Wait -NoNewWindow -PassThru | ForEach-Object {
-    if ($_.ExitCode -ne 0) {
-        Write-Host "Error: Lenovo Updater script failed with exit code $($_.ExitCode)"
-        exit $_.ExitCode
-    }
+write-host -ForegroundColor Cyan "Starting Invoke-PSLenovoUpdater..."
+write-host "Start-Process -FilePath `"C:\Program Files\PowerShell\7\pwsh.exe`" -ArgumentList `"-NoProfile -ExecutionPolicy Bypass -File `"$PSScriptRoot\Invoke-PSLenovoUpdater.ps1`"`""
+
+Start-Process -FilePath "C:\Program Files\PowerShell\7\pwsh.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSScriptRoot\Invoke-PSLenovoUpdater.ps1`"" -Wait -NoNewWindow -PassThru 
+if ($LASTEXITCODE -ne 0) {
+    Write-Host -ForegroundColor Red "Invoke-PSLenovoUpdater failed with exit code $LASTEXITCODE."
+    exit $LASTEXITCODE
+} else {
+    Write-Host -ForegroundColor Green "Invoke-PSLenovoUpdater completed successfully."
 }
