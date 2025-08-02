e2a9f8bc2c7b8be3ba995c009c3dd74e4ad8ea90
gwblok <gary@thebloks.com>
2025-08-01 23:04:17 -0500

    25.8.1.23.4

---
+++ DeployR/Catalog/Panasonic.ps1
@@ -0,0 +1,75 @@
+<# Top 10 models: 
+Internal Name | ModelAlias
+55mk3 | FZ55-3
+55mk2 | FZ55-2
+55Mk1 | FZ55-1
+40mk2 | FZ40-2
+40mk1 | FZ40-1
+33mk4 | CF33-4
+33mk3 | CF33-3
+33mk2 | CF33-2
+G2MK3 | FZG2-3
+G2MK2 | FZG2-2
+#>
+$PanasonicModels = @(
+    @{ ModelAlias = "FZ55-3"; URL10 = "https://na.panasonic.com/computer/cab/55GJ_Mk3_Win10_22H2_V3_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/55GJ_Mk3_Win11_24H2_V4_CAB.zip" }
+    @{ ModelAlias = "FZ55-2"; URL10 = "https://na.panasonic.com/computer/cab/55DEF_Mk2_Win10_22H2_V4_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/55DEF_Mk2_Win11_24H2_V3_CAB.zip" }
+    @{ ModelAlias = "FZ55-1"; URL10 = "https://na.panasonic.com/computer/cab/55ABC_Mk1_Win10_22H2_V4_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/55ABC_Mk1_Win11_24H2_V3_CAB.zip" }
+    @{ ModelAlias = "FZ40-2"; URL10 = "https://na.panasonic.com/computer/cab/40EFGH_Mk2_Win10_22H2_V1_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/40EFGH_Mk2_Win11_24H2_V4_CAB.zip" }
+    @{ ModelAlias = "FZ40-1"; URL10 = "https://na.panasonic.com/computer/cab/Win10_22H2/40ABCD_Mk1_Win10_22H2_V4_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/40ABCD_Mk1_Win11_24H2_V3_CAB.zip" }
+    @{ ModelAlias = "CF33-4"; URL10 = "https://na.panasonic.com/computer/cab/33Y890_Mk4_Win10_22H2_V1_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/33Y890_Mk4_Win11_24H2_V3_CAB.zip" }
+    @{ ModelAlias = "CF33-3"; URL10 = "https://na.panasonic.com/computer/cab/Win10_22H2/33TU45_Mk3_Win10_22H2_V2_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/33TU45_Mk3_Win11_24H2_V3_CAB.zip" }
+    @{ ModelAlias = "CF33-2"; URL10 = "https://na.panasonic.com/computer/cab/Win10_22H2/33GJKRS_Mk2_Win10_22H2_V3_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/33GJKRS_Mk2_Win11_24H2_V3_CAB.zip" }
+    @{ ModelAlias = "FZG2-3"; URL10 = "https://na.panasonic.com/computer/cab/G2NP_Mk3_Win10_22H2_V2_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/G2NP_Mk3_Win11_24H2_V4_CAB.zip" }
+    @{ ModelAlias = "FZG2-2"; URL10 = "https://na.panasonic.com/computer/cab/Win10_22H2/G2EGKM_Mk2_Win10_22H2_V3_CAB.zip"; URL11 = "https://na.panasonic.com/computer/cab/G2EGKM_Mk2_Win11_24H2_V3_CAB.zip" }
+)
+
+$JSON = @'
+{
+    "PanasonicModels": {
+        "FZ55-3": {
+            "URL10": "https://na.panasonic.com/computer/cab/55GJ_Mk3_Win10_22H2_V3_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/55GJ_Mk3_Win11_24H2_V4_CAB.zip"
+        },
+        "FZ55-2": {
+            "URL10": "https://na.panasonic.com/computer/cab/55DEF_Mk2_Win10_22H2_V4_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/55DEF_Mk2_Win11_24H2_V3_CAB.zip"
+        },
+        "FZ55-1": {
+            "URL10": "https://na.panasonic.com/computer/cab/55ABC_Mk1_Win10_22H2_V4_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/55ABC_Mk1_Win11_24H2_V3_CAB.zip"
+        },
+        "FZ40-2": {
+            "URL10": "https://na.panasonic.com/computer/cab/40EFGH_Mk2_Win10_22H2_V1_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/40EFGH_Mk2_Win11_24H2_V4_CAB.zip"
+        },
+        "FZ40-1": {
+            "URL10": "https://na.panasonic.com/computer/cab/Win10_22H2/40ABCD_Mk1_Win10_22H2_V4_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/40ABCD_Mk1_Win11_24H2_V3_CAB.zip"
+        },
+        "CF33-4": {
+            "URL10": "https://na.panasonic.com/computer/cab/33Y890_Mk4_Win10_22H2_V1_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/33Y890_Mk4_Win11_24H2_V3_CAB.zip"
+        },
+        "CF33-3": {
+            "URL10": "https://na.panasonic.com/computer/cab/Win10_22H2/33TU45_Mk3_Win10_22H2_V2_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/33TU45_Mk3_Win11_24H2_V3_CAB.zip"
+        },
+        "CF33-2": {
+            "URL10": "https://na.panasonic.com/computer/cab/Win10_22H2/33GJKRS_Mk2_Win10_22H2_V3_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/33GJKRS_Mk2_Win11_24H2_V3_CAB.zip"
+        },
+        "FZG2-3": {
+            "URL10": "https://na.panasonic.com/computer/cab/G2NP_Mk3_Win10_22H2_V2_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/G2NP_Mk3_Win11_24H2_V4_CAB.zip"
+        },
+        "FZG2-2": {
+            "URL10": "https://na.panasonic.com/computer/cab/Win10_22H2/G2EGKM_Mk2_Win10_22H2_V3_CAB.zip",
+            "URL11": "https://na.panasonic.com/computer/cab/G2EGKM_Mk2_Win11_24H2_V3_CAB.zip"
+        }
+    }
+}
+'@
+
+$WorkingPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$Data = $JSON | Out-File "$WorkingPath\Panasonic.json"
