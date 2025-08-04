7945109bbb00d0058754f43ccc2eab1a778ead81
gwblok <gary@thebloks.com>
2025-08-04 12:30:03 -0500

    25.8.4.12.29

--- DeployR/CustomSteps/ReferencedContent/CustomStepDefinitionsScripts-d819ff51-72a1-4e3c-bdf3-fcb69cf1bbab/d819ff51-72a1-4e3c-bdf3-fcb69cf1bbab/1/Set-InitialVariables.ps1
+++ DeployR/CustomSteps/ReferencedContent/CustomStepDefinitionsScripts-d819ff51-72a1-4e3c-bdf3-fcb69cf1bbab/d819ff51-72a1-4e3c-bdf3-fcb69cf1bbab/1/Set-InitialVariables.ps1
@@ -12,9 +12,17 @@ Write-Host "====================================================================
 
 # Get the provided variables
 [String]$SetTimeZoneName = ${TSEnv:SetTimeZoneName}
+[String]$TimeZoneDropDown = ${TSEnv:TimeZoneDropDown}
 [String]$InitialProgressTimeout = ${TSEnv:InitialProgressTimeout}
 [String]$InitialPeering = ${TSEnv:InitialPeering}
 [String]$InitialFinishAction = ${TSEnv:InitialFinishAction}
+
+[String]$InitialSystemLocale = ${TSEnv:InitialSystemLocale}
+[String]$InitialUserLocale = ${TSEnv:InitialUserLocale}
+[String]$InitialUILanguage = ${TSEnv:InitialUILanguage}
+[String]$InitialInputLocale = ${TSEnv:InitialInputLocale}
+
+
 
 Write-Host "Recording initial variables for the Task Sequence environment"
 #Report Variables:
@@ -22,11 +30,16 @@ Write-Output "Var SetTimeZoneName: $SetTimeZoneName"
 Write-Output "Var InitialProgressTimeout: $InitialProgressTimeout"
 Write-Output "Var InitialPeering: $InitialPeering"
 Write-Output "Var InitialFinishAction: $InitialFinishAction"
+Write-Output "Var InitialSystemLocale: $InitialSystemLocale"
+Write-Output "Var InitialUserLocale: $InitialUserLocale"
+Write-Output "Var InitialUILanguage: $InitialUILanguage"
+Write-Output "Var InitialInputLocale: $InitialInputLocale"
 Write-Host "================================================================================"
 write-host "Doing the work...."
 
 ${TSEnv:OSDStartTime} = (Get-Date).ToUniversalTime().ToString('yyyy-MM-dd HH:mm:ss')
 Write-Host "Setting OSDStartTime to: ${TSEnv:OSDStartTime}" -ForegroundColor Green
+
 
 if ($SetTimeZoneName -ne "") {
     Write-Output "Setting Time Zone Name to: $SetTimeZoneName"
@@ -45,6 +58,22 @@ if ($SetTimeZoneName -ne "") {
 } else {
     Write-Output "No Time Zone Name provided. Skipping time zone setting."
 }
+if ($TimeZoneDropDown -ne "") {
+    Write-Output "Setting Time Zone Name to: $TimeZoneDropDown"
+    ${TSEnv:TimeZone} = $TimeZoneDropDown
+    if ($env:SystemDrive -eq "X:") {
+        Write-Output "Running in WinPE, set TIMEZONE Variable for DeployR to add to unattended.xml"
+    }
+    else {
+        try {
+            Set-TimeZone -Id $TimeZoneDropDown
+        } catch {
+            Write-Output "Failed to set Time Zone Name $TimeZoneDropDown | $_"
+        }
+    }
+} else {
+    Write-Output "No Time Zone Drop Down provided. Skipping time zone drop down setting."
+}
 
 if ($InitialProgressTimeout -ne "") {
     Write-Output "Setting Progress Timeout to: $InitialProgressTimeout"
@@ -54,12 +83,13 @@ else {
     Write-Output "No Progress Timeout provided. Skipping progress timeout setting."
 }
 
-if ($InitialPeering -ne "") {
+if ($InitialPeering -eq "True") {
     Write-Output "Setting Peering to: $InitialPeering"
-    ${TSEnv:Peering} = $InitialPeering
+    #${TSEnv:Peering} = $InitialPeering - Don't need to set this, it is already set in the Task Sequence environment by default
 }
 else {
-    Write-Output "No Peering provided. Skipping peering setting."
+    Write-Output "Setting Peering to: $InitialPeering"
+    ${TSEnv:Peering} = $InitialPeering
 }
 
 if ($InitialFinishAction -ne "") {
@@ -72,6 +102,36 @@ if ($InitialFinishAction -ne "") {
 }
 else {
     Write-Output "No Finish Action provided. Skipping finish action setting."
+}
+
+if ($InitialSystemLocale -ne "") {
+    Write-Output "Setting System Locale to: $InitialSystemLocale"
+    ${TSEnv:SystemLocale} = $InitialSystemLocale
+}
+else {
+    Write-Output "No System Locale provided. Skipping system locale setting."
+}
+if ($InitialUserLocale -ne "") {
+    Write-Output "Setting User Locale to: $InitialUserLocale"
+    ${TSEnv:UserLocale} = $InitialUserLocale
+}
+else {
+    Write-Output "No User Locale provided. Skipping user locale setting."
+}
+if ($InitialUILanguage -ne "") {
+    Write-Output "Setting UI Language to: $InitialUILanguage"
+    ${TSEnv:UILanguage} = $InitialUILanguage
+}
+else {
+    Write-Output "No UI Language provided. Skipping UI language setting."
+}
+if ($InitialInputLocale -ne "") {
+    $InitialInputLocale = ($InitialInputLocale.Split('-')[1]).TrimStart()
+    Write-Output "Setting Input Locale to: $InitialInputLocale"
+    ${TSEnv:InputLocale} = $InitialInputLocale
+}
+else {
+    Write-Output "No Input Locale provided. Skipping input locale setting."
 }
 
 #Function to get full OS Build & UBR
