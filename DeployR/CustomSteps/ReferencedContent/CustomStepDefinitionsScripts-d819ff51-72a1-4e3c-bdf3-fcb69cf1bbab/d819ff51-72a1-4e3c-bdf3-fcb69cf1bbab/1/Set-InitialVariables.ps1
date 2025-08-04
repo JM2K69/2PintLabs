080cef4fa6d237388cf9ca070b60e8d8d81cfaba
gwblok <gary@thebloks.com>
2025-08-04 13:37:58 -0500

    Cleanup

--- DeployR/CustomSteps/ReferencedContent/CustomStepDefinitionsScripts-d819ff51-72a1-4e3c-bdf3-fcb69cf1bbab/d819ff51-72a1-4e3c-bdf3-fcb69cf1bbab/1/Set-InitialVariables.ps1
+++ DeployR/CustomSteps/ReferencedContent/CustomStepDefinitionsScripts-d819ff51-72a1-4e3c-bdf3-fcb69cf1bbab/d819ff51-72a1-4e3c-bdf3-fcb69cf1bbab/1/Set-InitialVariables.ps1
@@ -2,6 +2,13 @@
 <#  
 This should be one of the very first steps in the Task Sequence, before you even apply the image.
 
+This step records the initial variables for the Task Sequence environment, such as Time Zone, Progress Timeout, Peering, Finish Action, and Locale settings.
+
+It also records the OSDStartTime in UTC format, which can be used for logging and other purposes.
+
+It also records the WinPE Build Info, which is useful for troubleshooting and logging purposes.
+
+
 #>
 Import-Module DeployR.Utility
 
@@ -27,6 +34,7 @@ Write-Host "====================================================================
 Write-Host "Recording initial variables for the Task Sequence environment"
 #Report Variables:
 Write-Output "Var SetTimeZoneName: $SetTimeZoneName"
+Write-Output "Var TimeZoneDropDown: $TimeZoneDropDown"
 Write-Output "Var InitialProgressTimeout: $InitialProgressTimeout"
 Write-Output "Var InitialPeering: $InitialPeering"
 Write-Output "Var InitialFinishAction: $InitialFinishAction"
@@ -40,24 +48,9 @@ write-host "Doing the work...."
 ${TSEnv:OSDStartTime} = (Get-Date).ToUniversalTime().ToString('yyyy-MM-dd HH:mm:ss')
 Write-Host "Setting OSDStartTime to: ${TSEnv:OSDStartTime}" -ForegroundColor Green
 
+#Time Zone Data: https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/default-time-zones?view=windows-11
 
-if ($SetTimeZoneName -ne "") {
-    Write-Output "Setting Time Zone Name to: $SetTimeZoneName"
-    ${TSEnv:TimeZone} = $SetTimeZoneName
-    if ($env:SystemDrive -eq "X:") {
-        Write-Output "Running in WinPE, set TIMEZONE Variable for DeployR to add to unattended.xml"
-    }
-    else {
-        try {
-            Set-TimeZone -Id $SetTimeZoneName
-        } catch {
-            Write-Output "Failed to set Time Zone Name $SetTimeZoneName | $_"
-        }
-    }
-
-} else {
-    Write-Output "No Time Zone Name provided. Skipping time zone setting."
-}
+#Set the Time Zone based on the provided variables in the Drop Down Box
 if ($TimeZoneDropDown -ne "") {
     Write-Output "Setting Time Zone Name to: $TimeZoneDropDown"
     ${TSEnv:TimeZone} = $TimeZoneDropDown
@@ -75,6 +68,26 @@ if ($TimeZoneDropDown -ne "") {
     Write-Output "No Time Zone Drop Down provided. Skipping time zone drop down setting."
 }
 
+#Set the Time Zone based on the provided variable in the Text Box, this comes second to the Drop Down Box to ensure if this is set, it overrides the Drop Down Box
+if ($SetTimeZoneName -ne "") {
+    Write-Output "Setting Time Zone Name to: $SetTimeZoneName"
+    ${TSEnv:TimeZone} = $SetTimeZoneName
+    if ($env:SystemDrive -eq "X:") {
+        Write-Output "Running in WinPE, set TIMEZONE Variable for DeployR to add to unattended.xml"
+    }
+    else {
+        try {
+            Set-TimeZone -Id $SetTimeZoneName
+        } catch {
+            Write-Output "Failed to set Time Zone Name $SetTimeZoneName | $_"
+        }
+    }
+
+} else {
+    Write-Output "No Time Zone Name provided. Skipping time zone setting."
+}
+
+#Set the Progress Timeout, this is used to set the timeout for the progress bar at the end of the Task Sequence
 if ($InitialProgressTimeout -ne "") {
     Write-Output "Setting Progress Timeout to: $InitialProgressTimeout"
     ${TSEnv:ProgressTimeout} = $InitialProgressTimeout
@@ -83,6 +96,8 @@ else {
     Write-Output "No Progress Timeout provided. Skipping progress timeout setting."
 }
 
+#Set the Peering, this is used to set the peering for the Task Sequence
+#If the Peering is set to True, it will not set the variable, as it is already set in the Task Sequence environment by default
 if ($InitialPeering -eq "True") {
     Write-Output "Setting Peering to: $InitialPeering"
     #${TSEnv:Peering} = $InitialPeering - Don't need to set this, it is already set in the Task Sequence environment by default
@@ -92,6 +107,7 @@ else {
     ${TSEnv:Peering} = $InitialPeering
 }
 
+#Set the Finish Action, this is used to set the finish action for the Task Sequence
 if ($InitialFinishAction -ne "") {
     if ($InitialFinishAction -eq "blank") {
         # Don't set this at all if left blank, don't even set it to null, that will break it too
@@ -104,6 +120,9 @@ else {
     Write-Output "No Finish Action provided. Skipping finish action setting."
 }
 
+#Sets the Launguage and Locale settings for the Task Sequence environment
+# https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/available-language-packs-for-windows?view=windows-11
+# https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/default-input-locales-for-windows-language-packs?view=windows-11
 if ($InitialSystemLocale -ne "") {
     Write-Output "Setting System Locale to: $InitialSystemLocale"
     ${TSEnv:SystemLocale} = $InitialSystemLocale
