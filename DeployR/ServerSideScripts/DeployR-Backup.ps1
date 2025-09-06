5c997d8273d8348a89a4f7c204e88c4c6886fbae
gwblok <gary@thebloks.com>
2025-09-06 00:52:47 -0500

    25.9.6.0.52

--- DeployR/ServerSideScripts/DeployR-Backup.ps1
+++ DeployR/ServerSideScripts/DeployR-Backup.ps1
@@ -55,7 +55,7 @@ if (-not (Test-Path -Path "$BackupLocation\$DateStamp\TaskSequences")) {New-Item
 Write-Host "Starting DeployR backup at $DateStamp" -ForegroundColor Green
 #Backup DeployR content items
 Write-Host "Backing up DeployR content items..." -ForegroundColor Yellow
-$ContentItems = Get-DeployRContentItem | Where-Object {$_.id -notlike '00000000-0000-0000-0000-*'} | Where-Object {$_.contentItemPurpose -match "Other"}
+$ContentItems = Get-DeployRContentItem | Where-Object {$_.id -notlike '00000000-0000-0000-0000-*'} | Where-Object {$_.contentItemPurpose -match "Other"} | Where-Object {$_.name -notmatch "Test" -and $_.name -notmatch "DeployR client"}
 $ContentItems | ForEach-Object {
     write-host "Backing up content item: $($_.name) | $($_.id)" -ForegroundColor Cyan
     Export-DeployRContentItem -Id $_.id -DestinationFolder "$BackupLocation\$DateStamp\ContentItems\$($_.name)-$($_.id)"
@@ -130,7 +130,7 @@ if ($EnableBackup2GitHub -and $GitHubCustomSteps -and $GitHubCustomStepsReferenc
     }
     #Get all step definitions except the built-in ones
     write-host "Getting all step definitions..." -ForegroundColor Yellow
-    $StepDefinitions = Get-DeployRMetadata -Type StepDefinition | Where-Object {$_.id -notlike '0000*'}
+    $StepDefinitions = Get-DeployRMetadata -Type StepDefinition | Where-Object {$_.id -notlike '0000*' -and $_.name -notmatch "Delete"}
     foreach ($stepDef in $StepDefinitions) {
         write-host "Backing up step definition: $($stepDef.name) | $($stepDef.id)" -ForegroundColor Cyan
         $ExportFolderName = "$($stepDef.name)-$($stepDef.id)"
